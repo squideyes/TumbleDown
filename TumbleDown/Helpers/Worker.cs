0d30f3cba10040fb92cf291f3d640a48cd4ffea5
Louis S. Berman <louis@squideyes.com>
2018-08-16 17:25:36 -0400

    Updated license header

--- TumbleDown/Helpers/Worker.cs
+++ TumbleDown/Helpers/Worker.cs
@@ -1,4 +1,21 @@
-﻿using Microsoft.Extensions.CommandLineUtils;
+﻿// Copyright 2018 Louis S.Berman.
+//
+// This file is part of TumbleDown.
+//
+// TumbleDown is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published 
+// by the Free Software Foundation, either version 3 of the License, 
+// or (at your option) any later version.
+//
+// TumbleDown is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU 
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with TumbleDown.  If not, see <http://www.gnu.org/licenses/>.
+
+using Microsoft.Extensions.CommandLineUtils;
 using Microsoft.Extensions.Logging;
 using System;
 
